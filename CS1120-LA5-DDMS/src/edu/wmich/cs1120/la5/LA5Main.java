63f9bac67e52b191dddd5bc24627e43fa047e041
danie <danie@10.80.148.207>
2017-03-15 11:31:22 -0400

    Created Project Template

---
+++ CS1120-LA5-DDMS/src/edu/wmich/cs1120/la5/LA5Main.java
@@ -0,0 +1,16 @@
+/*
+ * Name: CS1120 LA5
+ * Authors: Daniel Desnoyer, Matthew Schutz
+ * Date: 3/15/17
+ * References: N/A
+ */
+package edu.wmich.cs1120.la5;
+
+import edu.wmich.cs1120.la5.gui.MainWindow;
+import edu.wmich.cs1120.la5.TerrainScanner;
+
+public class LA5Main {
+	
+	//MainWindow.startGUI();
+
+}
