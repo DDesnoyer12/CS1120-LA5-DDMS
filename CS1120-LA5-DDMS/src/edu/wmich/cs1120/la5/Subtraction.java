2fe3b4f72871320fb9ff6534fc4eeddf20d643f2
Daniel <danie@10.80.143.179>
2017-03-22 11:01:41 -0400

    Implemented the perform method and created Javadoc tags

--- CS1120-LA5-DDMS/src/edu/wmich/cs1120/la5/Subtraction.java
+++ CS1120-LA5-DDMS/src/edu/wmich/cs1120/la5/Subtraction.java
@@ -2,10 +2,19 @@ package edu.wmich.cs1120.la5;
 
 public class Subtraction implements IOperation {
 
+	/**
+	 * Performs a subtraction equation using the values of two
+	 * IExpression objects.
+	 * @param left The IExpression on the left side of the equation.
+	 * 
+	 * @param right The IExpression on the right side of the equation
+	 * 
+	 * @return Returns the difference between the values of two 
+	 * IExpression objects.
+	 */
 	@Override
 	public Integer perform(IExpression left, IExpression right) {
-		// TODO Auto-generated method stub
-		return null;
+		return (left.getValue() - right.getValue());
 	}
 
 }
