17e7cd390b89eab45eb1faa8dccb7654432163f5
Daniel <danie@10.80.143.179>
2017-03-22 11:00:59 -0400

    Implemented the perform method and created Javadoc tags.

--- CS1120-LA5-DDMS/src/edu/wmich/cs1120/la5/Addition.java
+++ CS1120-LA5-DDMS/src/edu/wmich/cs1120/la5/Addition.java
@@ -2,10 +2,18 @@ package edu.wmich.cs1120.la5;
 
 public class Addition implements IOperation {
 
+	/**
+	 * Performs an addition equation using the values of two 
+	 * IExpression objects.
+	 * @param left The Expression on the left side of the equation.
+	 * 
+	 * @param right The Expression on the right side of the equation.
+	 * 
+	 * @return Returns the sum of the values of both Expressions
+	 */
 	@Override
 	public Integer perform(IExpression left, IExpression right) {
-		// TODO Auto-generated method stub
-		return null;
+		return (left.getValue() + right.getValue());
 	}
 
 }
